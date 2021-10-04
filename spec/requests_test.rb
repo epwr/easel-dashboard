f4da372933c89708dd7cf6b8dc93699fc09c98f3
epwr <ericpower@outlook.com>
2021-10-03 22:26:38 -0700

    v0.6 - added basic request framework for tests

---
+++ spec/requests_test.rb
@@ -0,0 +1,50 @@
+#!/snap/bin/ruby
+#
+# Author: Eric Power
+#
+# Description:
+#     Tests that requests from a client (eg. a web browser) work properly.
+
+# Imports
+require_relative '../lib/easel.rb'
+require 'open3'
+
+# Key Variables
+PATH_TO_YAML = "#{__FILE__}/../docs/examples/dash-1.yaml"
+
+describe RequestTests do
+
+
+    before(:context) do
+      # Create a new server on localhost:4211 (for every context)
+      @saved_ARGV = ARGV
+      ARGV = [PATH_TO_YAML]
+      @server_pid = fork do
+        launch
+      end
+      puts "FOUND PID: #{@server_pid}" # TODO: make sure this holds the actual pid.
+    end
+
+   context "The TCP Server" do
+
+
+
+      it "should accept TCP connections" do
+
+      end
+
+      it "should close TCP connections after a non-HTTP formated string is sent." do
+         msg "this is not HTTP - idk what this is."
+         # TODO:
+      end
+
+   end
+
+
+
+
+   after(:context) do
+     # Kill the server after every context.
+     # TODO: Kill the server using @server_pid
+   end
+end
