8f35ba67288f6f602e6ee20b37794c582f4f8dd8
epwr <ericpower@outlook.com>
2021-10-13 15:29:49 -0700

    v0.7 - splitting websocket.rb into websocket_server and websocket_event_handler

---
+++ lib/easel/websocket_event_handler.rb
@@ -0,0 +1,8 @@
+#!/bin/env ruby
+#
+# Author: Eric Power
+#
+# Description:
+#     Contains functions that are called by the websocket_server in response to
+#     messages that are received from the client, and helping methods (including
+#     functions that tell the websocket_server to send particular responses).
